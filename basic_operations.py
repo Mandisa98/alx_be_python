4b0a4b63b98eab2176234827cde2a0490a2f46f1
Mandisa <ndindi.mm@gmail.com>
2025-07-19 19:21:56 +0200

    Resolve merge conflict in basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,3 +1,4 @@
+
 number1 = 10
 number2 = 5
 
@@ -9,3 +10,16 @@ print("Addition of", number1, "and", number2, "is", sum_result)
 print("Subtraction of", number1, "and", number2, "is", difference)
 print("Multiplication of", number1, "and", number2, "is", product)
 
+=======
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
+
+>>>>>>> Stashed changes
